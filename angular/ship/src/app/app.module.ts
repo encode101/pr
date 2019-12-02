3e123761ac75499ebd458b18bb9ee46749ab2d81
Rahul Mishra <rahul@rahulmishra.com>
2019-12-02 23:05:28 +0530

    interim commit

--- angular/ship/src/app/app.module.ts
+++ angular/ship/src/app/app.module.ts
@@ -2,21 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-
+import { UsersComponent } from './users/users.component';
+import { ProfileComponent } from './profile/profile.component';
+const appRoutes = [
+  { path: '', component: ProfileComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'profile', component: ProfileComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    UsersComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- angular/ship/src/app/server/server.component.ts
+++ angular/ship/src/app/server/server.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
-    selector: '.app-server',
+    selector: '[app-server]',
     templateUrl: './server.component.html',
     styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+    blogTitle: string = 'Google';
+    serverStatus: boolean = false;
+    serverStatusText = "No Server Was Created";
+    serverName: string;
+
+    createServer = () => {
+        this.serverStatus = !this.serverStatus;
+        if (this.serverStatus) {
+            this.serverStatusText = "Server Was Not Created";
+        } else {
+            this.serverStatusText = "Server Was Created";
+        }
+    }
+
+    createNewServer(e: any){
+        this.serverName = e.target.value;
+    }
+
 }
 
